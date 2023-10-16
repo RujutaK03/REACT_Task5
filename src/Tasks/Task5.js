3982dae355120689679e8fb944c5a198fc42cec5
RujutaK03 <111700895+RujutaK03@users.noreply.github.com>
2023-10-16 21:16:47 +0530

    Update Task5.js

--- src/Tasks/Task5.js
+++ src/Tasks/Task5.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 class Task5 extends Component {
     constructor(props) {
